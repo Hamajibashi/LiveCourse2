16c0d0490385d2fd69c93be959dd8029766bade5
Mylène <andreasakata@gmail.com>
2022-02-22 02:07:41 +0800

    新增 登入功能 以及 簡易產品頁面

---
+++ login.js
@@ -0,0 +1,33 @@
+import {createApp} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.prod.min.js';
+
+const url = 'https://vue3-course-api.hexschool.io/v2';
+          const path = 'mylmii';
+
+          const usernameInput = document.querySelector("#username");
+          const pwdInput = document.querySelector("#password");
+          const loginBtn = document.querySelector("#login");
+
+loginBtn.addEventListener('click', login);
+function login(){
+    
+    const username = usernameInput.value;
+    const password = pwdInput.value;
+
+    const user = {
+        username,
+        password
+    }
+    console.log(username, password);
+
+    axios.post(`${url}/admin/signin`,user)
+    .then( res=>{
+        console.log(res);
+        const {token,expired} = res.data;
+        console.log(token,expired);
+        document.cookie = `mylToken=${token}; expires=${new Date(expired)};`;
+        window.location="products.html"
+    })
+    .catch( err=>{
+        console.dir(err);
+    })
+}
